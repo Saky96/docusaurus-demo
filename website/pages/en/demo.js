6b7773bd71bac93b0ad48881dfd5a93b205d8a41
Saky96 <saigalsaksham96@gmail.com>
2020-06-07 00:21:57 +0530

    project setup

---
+++ website/pages/en/demo.js
@@ -0,0 +1,12 @@
+const React = require('react');
+class Demo extends React.Component {
+    render(){
+        return (
+            <div>
+                hi hi
+            </div>
+        )
+    }
+}
+
+module.exports = Demo;
